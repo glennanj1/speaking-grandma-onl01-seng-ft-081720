2733dd6a91f39e384154ec65dc95203acf7aad57
John Glennan <glennan20@gmail.com>
2020-08-18 14:51:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,8 +2,6 @@
 def speak_to_grandma(phrase = "HUH?! SPEAK UP, SONNY!")
   if phrase == phrase.upcase
     return("NO, NOT SINCE 1938!")
-  elsif phrase == phrase.downcase
-    return("Huh?! SPEAK UP, SONNY!")
   elsif phrase === 'I LOVE YOU GRANDMA!'
     return("I LOVE YOU TOO PUMPKIN!")
   else
